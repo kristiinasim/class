d8e6d0c412d2fd374f67803e15602422b62f3692
kristiinasim <33310254+kristiinasim@users.noreply.github.com>
2017-12-05 09:54:54 +0200

    pank

---
+++ pank/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pank
+{
+    class Program
+    {
+        static void Main(string[] args)
+           
+        {
+            
+           
+            konto1 konto = new konto1();
+            Console.WriteLine("Palun registreerige endale kontonimi:");
+            konto.nimi = Console.ReadLine();
+            Console.WriteLine("Nüüd registreerige palun PIN-kood:");
+            konto.pin = int.Parse(Console.ReadLine());
+            konto.kontoseis = 444;
+
+            Start:
+            Console.WriteLine("Mida soovite edasi teha? Kas kontoseisu näha (n), raha välja võtta(-)või juurde lisada (+)? Väljumiseks vajutage e tähte");
+            string valik = Console.ReadLine();
+
+            if (valik == "+")
+            {
+                Console.WriteLine("Kui palju raha soovite lisada (eurodes)?");
+                int raha = int.Parse(Console.ReadLine());
+                konto.kontoseis = konto.kontoseis + raha;
+                Console.WriteLine("Aitäh! Teie arvel on nüüd " + konto.kontoseis + " eurot");
+                Console.ReadLine();
+                goto Start;
+
+
+
+            }
+            if (valik == "-")
+            {
+                Console.WriteLine("Kui palju raha soovite kontolt välja võtta (eurodes)?");
+                int raha = int.Parse(Console.ReadLine());
+                konto.kontoseis = konto.kontoseis - raha;
+                Console.WriteLine("Aitäh! Teie arvel on nüüd " + konto.kontoseis + " eurot");
+                Console.ReadLine();
+                goto Start;
+
+            }
+            if (valik == "n")
+            {
+            Console.WriteLine("Teie arvel on " + konto.kontoseis + " eurot");
+            goto Start;
+        }
+        if (valik == "e")
+        {
+                System.IO.File.WriteAlltext((@"C: \Users\opilane\Desktop\pank.txt"), konto.nimi);
+                System.IO.File.WriteAlltext((@"C: \Users\opilane\Desktop\pank.txt"), konto.pin);
+                System.IO.File.WriteAlltext((@"C: \Users\opilane\Desktop\pank.txt"), konto.kontoseis);
+
+
+
+
+
+
+            }
+        }
+
+
+
+
+
+
+
+
---
+++ pank/konto1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pank
+{
+    class konto1
+    {
+        public string nimi { get; set; }
+        public int pin { get; set; }
+        public int rahasummasisse { get; set; }
+        public int rahasummavälja { get; set; }
+        public int kontoseis { get; set; }
+        public int kontoseis1 { get; set; }
+        public string valik { get; set; }
+
+        //public void rahasisse(int raha)
+        //{
+        //    kontoseis = kontoseis1 + raha;
+        //    kontoseis1 = 444;
+        //    raha = int.Parse(Console.ReadLine());
+
+
+        //}
+        //public string kontoseis = System.IO.File.ReadAllText(@"C:\Users\opilane\Desktop\pank.txt");
+
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
