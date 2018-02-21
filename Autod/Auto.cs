c0352b2d89c1035420a3d94e633d7e923a9ce5d6
kristiinasim <33310254+kristiinasim@users.noreply.github.com>
2018-02-21 22:19:01 +0200

    LINQ vale

---
+++ Autod/Auto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Autod
+{
+    class Auto
+    {
+        public string Mudel { get; set; }
+        public int KW { get; set; }
+        public string Tootja { get; set; }
+        public string Värv { get; set; }
+    }
+}
