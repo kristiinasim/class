ca0e5df1144d8eb6adbe846fd5ca92f7db0e02c0
kristiinasim <33310254+kristiinasim@users.noreply.github.com>
2018-05-15 21:03:31 +0300

    xml base, totally wrong

---
+++ XML - note/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+using System.Xml.Serialization;
+
+namespace XML___note
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            XmlReader xmlReader = XmlReader.Create("http://www.ecb.int/stats/eurofxref/eurofxref-daily.xml");
+            Console.WriteLine("Mida soovite teha?\n1.Märget luua.\n2.Märget lugeda.\n3.Märget kustutada");
+            string choice = Console.ReadLine();
+            var chc = new List<Note>();
+            XmlElement element1 = doc.CreateElement(string.Empty, "body", string.Empty);
+            doc.AppendChild(element1);
+        }
+    }
+}
