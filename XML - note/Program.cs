9f41283d8db880c7ff0314c9e8c0c21422f3107e
kristiinasim <33310254+kristiinasim@users.noreply.github.com>
2018-05-31 09:54:40 +0300

    Kustuta töötab, Loo töötab, muuda vigane

--- XML - note/Program.cs
+++ XML - note/Program.cs
@@ -7,6 +7,7 @@ using System.Xml;
 using System.Xml.Linq;
 using System.Xml.Serialization;
 using System.IO;
+
 namespace XML___note
 {
     class Program
@@ -17,80 +18,78 @@ namespace XML___note
             while (true)
 
             {
-                Console.WriteLine("Mida soovite teha?\n1.Märget luua. (Loo)\n2.Märget muuta. (Muuda)\n3.Märget kustutada. (Kustuta)");
-             
+                Console.WriteLine("Mida soovite teha?\n1.Märget luua. (Loo)\n2.Märget muuta. (Muuda)\n3.Märget kustutada. (Kustuta) \n4.Programmist väljuda. (V2lju)");
+
                 string choice = Console.ReadLine();
-               
+
 
                 //XmlElement element1 = doc.CreateElement(string.Empty, "body", string.Empty);
                 //doc.AppendChild(element1);
+                #region Loo
 
                 if (choice == "Loo")
-
+                {
                     Console.WriteLine("Pealkiri:");
 
-                var title = Console.ReadLine();
+                    var title = Console.ReadLine();
 
-            using (XmlWriter writer = XmlWriter.Create(@"C:\Users\Krizzie\Documents\GitHub\class\XML - note\" + title + ".xml"))
+                    using (XmlWriter writer = XmlWriter.Create(@"C:\Users\Krizzie\Documents\GitHub\class\XML - note\" + title + ".xml"))
 
-            {
+                    {
 
-                writer.WriteStartDocument();
-                writer.WriteStartElement(title);
-                writer.WriteStartElement("Pealkiri", title);
-                //if (doc.DocumentElement.Attributes["Pealkiri"] != null)
-                //    Console.WriteLine(doc.DocumentElement.Attributes["Pealkiri"].Value);
-                Console.WriteLine("Sisu:");
-                writer.WriteElementString("Sisu", Console.ReadLine());
+                        writer.WriteStartDocument();
+                        writer.WriteStartElement(title);
+                        writer.WriteStartElement("Pealkiri", title);
+                        //if (doc.DocumentElement.Attributes["Pealkiri"] != null)
+                        //    Console.WriteLine(doc.DocumentElement.Attributes["Pealkiri"].Value);
+                        Console.WriteLine("Sisu:");
+                        writer.WriteElementString("Sisu", Console.ReadLine());
 
-                //if (doc.DocumentElement.Attributes["Sisu"] != null)
-                //    Console.WriteLine(doc.DocumentElement.Attributes["Sisu"].Value);
+                        //if (doc.DocumentElement.Attributes["Sisu"] != null)
+                        //    Console.WriteLine(doc.DocumentElement.Attributes["Sisu"].Value);
 
-                writer.WriteEndElement();
-                doc.Save(writer);
-                Console.WriteLine("Aitäh! Fail salvestatud! Kas soovite veel midagi teha? y/n");
-                var valik = Console.ReadLine();
-                if (valik == "y")
+                        writer.WriteEndElement();
+                        doc.Save(writer);
+                        Console.WriteLine("Aitäh! Fail salvestatud! Kas soovite veel midagi teha? y/n");
+                        var valik = Console.ReadLine();
+                        if (valik == "y")
+                        {
+                            continue;
+                        }
+                        else if (valik == "n")
+                        {
+                            break;
+                        }
+                    }
+                }
+                #endregion
+
+                #region Kustuta
+
+                if (choice == "Kustuta")
+                { Console.WriteLine("Millist faili soovite kustutada?");
+                    var failinimi = Console.ReadLine();
+                    File.Delete(@"C:\Users\Krizzie\Documents\GitHub\class\XML - note\" + failinimi + ".xml");
+                    Console.WriteLine("Aitäh! Fail on kustutatud!");
+                }
+                #endregion
+
+                if (choice == "Muuda")
                 {
-                    continue;
+                    Console.WriteLine("Millist faili soovite muuta?");
+                    var fail = Console.ReadLine();
+                    var pathname = (@"C:\Users\Krizzie\Documents\GitHub\class\XML - note\" + fail + ".xml");
+                    doc.Load(pathname);
+                    doc.SelectSingleNode("/appSettings/add").Attributes["Pealkiri"].Value = Console.ReadLine();
+                    doc.SelectSingleNode("/appSettings/add").Attributes["Sisu"].Value = Console.ReadLine();
+                    doc.Save(fail);
+
                 }
-                else if (valik == "n")
-                {
-                    break;
-                }
-            }
-                }
-
-
-
-            //   else 
-            //   {
-            //        Console.WriteLine("Midagi on valesti! Proovige uuesti!");
-
-
-            //    }
-            //}
-
-            //if (choice == "Loe")
-            //{
-            //    Console.WriteLine("Millist faili lugeda soovite? Palun sisestage pealkiri!");
-            //    var title2 = Console.ReadLine();
-            //    string path = (@"C:\Users\Krizzie\Documents\GitHub\class\XML - note\" + title2 + ".xml");
-            //    doc.Load(path);
-
-            //}
-            var choice2 = Console.ReadLine();
-            if (choice2 == "Kustuta")
-            {
-                if (doc.DocumentElement.Attributes["Pealkiri"] != null)
-                    Console.WriteLine(doc.DocumentElement.Attributes["Pealkiri"].Value);
-                Console.WriteLine("Millist faili soovite kustutada?");
-                var failinimi = Console.ReadLine();
-                File.Delete(@"C:\Users\Krizzie\Documents\GitHub\class\XML - note\"+failinimi+".xml");
-                if (doc.DocumentElement.Attributes["Sisu"] != null)
-                    Console.WriteLine(doc.DocumentElement.Attributes["Sisu"].Value);
-
-
+                //if (choice == "V2lju")
+                //{
+                //    break;
+                //    Environment.ExitCode();
+                //}
             }
         }
 
