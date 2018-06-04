5f78a10dd05e1eb4a174a3ca3d83ed379a58346f
kristiinasim <33310254+kristiinasim@users.noreply.github.com>
2018-06-04 20:42:31 +0300

    Completed, added a summary

--- XML - note/Program.cs
+++ XML - note/Program.cs
@@ -12,109 +12,129 @@ namespace XML___note
 {
     class Program
     {
+        /// <summary>
+        /// Creates a while cicle first. Program asks user what they want to do. Each if cicle contains a corresponding void for the names choice.
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
-            XmlDocument doc = new XmlDocument();
             while (true)
 
             {
-                Console.WriteLine("Mida soovite teha?\n1.Märget luua. (Loo)\n2.Märget muuta. (Muuda)\n3.Märget kustutada. (Kustuta) \n4.Programmist väljuda. (V2lju)");
 
-                string choice = Console.ReadLine();
-                Console.WriteLine("Pealkiri:");
-
-                var title = Console.ReadLine();
- 
-
-                //XmlElement element1 = doc.CreateElement(string.Empty, "body", string.Empty);
-                //doc.AppendChild(element1);
-                #region Loo
-
-                if (choice == "Loo")
-                {
-                    
-                    ////System.IO.File.WriteAllText(@"C:\Users\Krizzie\Documents\GitHub\class\XML - note\Pealkirjad.txt", title);
-
-
-                    using (XmlWriter writer = XmlWriter.Create(@"C:\Users\Krizzie\Documents\GitHub\class\XML - note\" + title + ".xml"))
-
-                    {
-
-                        writer.WriteStartDocument();
-                        writer.WriteStartElement(title);
-                        writer.WriteStartElement("Pealkiri", title);
-                        Console.WriteLine("Sisu:");
-                        writer.WriteElementString("Sisu", Console.ReadLine());
-               
-                        writer.WriteEndElement();
-                        doc.Save(writer);
-                        Console.WriteLine("Aitäh! Fail salvestatud! Kas soovite veel midagi teha? y/n");
-                        var valik = Console.ReadLine();
-                        if (valik == "y")
-                        {
-                            continue;
-                        }
-                        else if (valik == "n")
-                        {
-                            break;
-                        }
-                    }
-                }
-                #endregion
-
-                #region Kustuta
-
-                if (choice == "Kustuta")
-                { Console.WriteLine("Millist faili soovite kustutada?");
-                    var failinimi = Console.ReadLine();
-                    File.Delete(@"C:\Users\Krizzie\Documents\GitHub\class\XML - note\" + failinimi + ".xml");
-                    Console.WriteLine("Aitäh! Fail on kustutatud!");
-                }
-                #endregion
-
-                if (choice == "Muuda")
-                {
-                    Console.WriteLine("Millist faili soovite muuta?");
-                    var fail = Console.ReadLine();
-                    var pathname = (@"C:\Users\Krizzie\Documents\GitHub\class\XML - note\" + fail + ".xml");
-                    XElement d = XElement.Load(pathname);
-
-                   
-                    int Count = 1;
-                    Console.WriteLine("Muuda pealkirja:");
-                    var pealkirim = Console.ReadLine();
-                    //XmlNode node = doc.SelectSingleNode("Pealkiri/" + fail);
-
-
-                    Console.WriteLine(Count + ". " + node.Attributes["Pealkiri"].Value);
-                    node.Attributes["Pealkiri"].Value = pealkirim;
-                    Count += 1;
-
-
-                    //XmlNode node = doc.SelectSingleNode("Root/Node/Element");
-                    //node.Attributes[0].Value = pealkirim;t.Element("level").Element("node1").Value = "";
-                    //d.Element(title).Value = pealkirim;
-                    //d.Element("level").Element("node3").Value = "";
-
-
-                    Console.WriteLine("Muuda sisu:");
-                    var sisum = Console.ReadLine();
-                    XmlNode node2 = doc.SelectSingleNode("Node/" + fail);
-                    //node2.Attributes[1].Value = sisum;
-                    
-                    
-                    doc.Save(fail);
-
-                }
-                if (choice == "V2lju")
-                {
-                    break;
-                    Environment.Exit(1);
-                }
+            Console.WriteLine("Mida soovite teha?\n1.Märget luua. (Loo)\n2.Märget muuta. (Muuda)\n3.Märget kustutada. (Kustuta) \n4.Programmist väljuda. (V2lju)");
+            string choice = Console.ReadLine();
+            if (choice == "Loo")
+            {
+                Loo();
             }
+            if (choice == "Kustuta")
+            {
+                Delete();
+            }
+            if (choice == "Muuda")
+            {
+                Change();
+            }
+            if (choice == "V2lju")
+            {
+                Exit();
+            }
+            else
+
+            {
+                Console.WriteLine("Tegite midagi valesti! Proovige palun uuesti!");
+            }
+
+           }
+
         }
 
+        /// <summary>
+        /// Creates an XML file, user can type a title and content.
+        /// </summary>
+        static void Loo()
+        {
+            XmlDocument doc = new XmlDocument();
+            Console.WriteLine("Pealkiri:");
+            var title = Console.ReadLine();
+            using (XmlWriter writer = XmlWriter.Create(@"C:\Users\Krizzie\Documents\GitHub\class\XML - note\" + title + ".xml"))
+            {
+
+                writer.WriteStartDocument();
+                writer.WriteStartElement(title);
+                writer.WriteStartElement("Pealkiri", title);
+                Console.WriteLine("Sisu:");
+                writer.WriteElementString("Sisu", Console.ReadLine());
+
+                writer.WriteEndElement();
+                doc.Save(writer);
+                Console.WriteLine("Aitäh! Fail salvestatud!");
+                var valik = Console.ReadLine();
+
+            }
+        }
+        /// <summary>
+        /// This void is for deleting files named by user.
+        /// </summary>
+        static void Delete()
+        {
+            
+            Console.WriteLine("Millist faili soovite kustutada?");
+            var failinimi = Console.ReadLine();
+            File.Delete(@"C:\Users\Krizzie\Documents\GitHub\class\XML - note\" + failinimi + ".xml");
+            Console.WriteLine("Aitäh! Fail on kustutatud!");
+        }
+
+        /// <summary>
+        /// This void is for changing file's title and content if requested by user.
+        /// </summary>
+
+        static void Change()
+        {
+            XmlDocument doc = new XmlDocument();
+            Console.WriteLine("Millist faili soovite muuta?");
+            var fail = Console.ReadLine();
+            var pathname = (@"C:\Users\Krizzie\Documents\GitHub\class\XML - note\" + fail + ".xml");
+            doc.Load(pathname);
+
+            Console.WriteLine("Muuda pealkirja:");
+            var pealkirim = Console.ReadLine();
+            XmlNode root = doc.DocumentElement;
+            XmlNode myNode = root.SelectSingleNode("Pealkiri");
+            if (myNode != null)
+            {
+                myNode.Value = pealkirim;
+                doc.Save(pathname);
+            }
+            //XmlNode node = doc.SelectSingleNode("Root/Node/Element");
+            //node.Attributes[0].Value = pealkirim;t.Element("level").Element("node1").Value = "";
+            //d.Element(title).Value = pealkirim;
+            //d.Element("level").Element("node3").Value = "";
+            Console.WriteLine("Muuda sisu:");
+            var sisum = Console.ReadLine();
+            XmlNode rootx = doc.DocumentElement;
+            XmlNode myNodex = root.SelectSingleNode("Sisu");
+
+            if (myNodex != null)
+            {
+                myNodex.Value = sisum;
+                doc.Save(pathname);
+            }
+            doc.Save(pathname);
+
+        }
+        /// <summary>
+        /// This void simply closes the application.
+        /// </summary>
+        static void Exit()
+        {
+
+            Environment.Exit(1);
+        }
     }
 }
+
+    
     
 
